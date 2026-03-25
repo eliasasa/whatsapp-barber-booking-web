75bee578bcb28d72450db2be982e7642bc766506
eliasasa <eliasnetoneto28@gmail.com>
2026-03-25 18:54:11 -0400

    feat(appointments): export appointments feature through barrel index

---
+++ src/features/appointments/index.ts
@@ -0,0 +1,3 @@
+export { AppointmentsList } from "./components/AppointmentsList";
+export { listAppointments } from "./api/listAppointments";
+export { cancelAppointment } from "./api/cancelAppointment";
