4914e6271b4fa31d0851c1041340fde8eaa1bb9a
eliasasa <eliasnetoneto28@gmail.com>
2026-05-03 21:59:27 -0400

    feat(bot-api): add bot API index/export

--- src/features/bot/api/index.ts
+++ src/features/bot/api/index.ts
@@ -1,2 +1,6 @@
 export { getGreetingMessage } from "./getGreetingMessage";
 export { updateGreetingMessage } from "./updateGreetingMessage";
+export { getBotState } from "./getBotState";
+export { setBotState } from "./setBotState";
+export { restartBot } from "./restartBot";
+export type { BotState } from "./getBotState";
